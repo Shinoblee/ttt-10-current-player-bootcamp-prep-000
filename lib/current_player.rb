4e2f620c4c2c09b8106c2ee7565911d651c0cd1c
Lee Simon <shinoblee@gmail.com>
2018-09-25 13:19:59 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,15 @@
+board = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
+
+def turn_count(board)
+  count = 0 
+  board.each do |i|
+    puts board.i
+    #if board.index == 'X' || board.index == 'O'
+     # count += 1 
+    #end
+  end
+  #return count
+end
+
+def current_player
+end
